32b5b2c53fe6274b30b25e858beb86113f3b7c68
Anya <anya@MacBook-Anya.local>
2022-08-14 14:03:12 +0300

    first homework added

---
+++ HomeWork/lecture-1/js/task4.js
@@ -0,0 +1,8 @@
+// const a = prompt("Введи число!"); // меняй тип на число
+// const b = prompt("В какую степень возвести твое число?");
+// alert("Вот, что получилочь: " + Math.pow((+a), (+b))); // ставь пробелы
+
+const number = prompt('Введи число!');
+const powNumber = prompt('В какую степень возвести это число?');
+
+alert(`Вот, что получилось: ${Math.pow(number, powNumber)}`);
